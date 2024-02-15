3b4e4895e0645a312d55bc32a74b3b73a51a4a16
Uttam Pradhan <85547505+uttamaPrh@users.noreply.github.com>
2024-02-15 22:43:05 +0545

    Update script.js

--- script.js
+++ script.js
@@ -286,4 +286,29 @@ function buildSeamlessLoop(items, spacing) {
   });
 
 }(jQuery));
+$.js = function (el) {
+  return $("[data-js=" + el + "]");
+};
 
+function carousel() {
+  $.js("timeline-carousel").slick({
+    infinite: false,
+    arrows: false,
+    dots: true,
+    autoplay: false,
+    speed: 1100,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+}
+
+carousel();
